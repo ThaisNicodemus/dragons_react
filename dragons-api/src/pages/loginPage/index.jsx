58942adc45d84c5dcd7914083eedbd6f61e93b5f
thais.nicodemus <thais.nicodemus@compasso.com.br>
2022-06-09 15:42:39 -0300

    iniciando as rotas de validação - teste de retorno de input

--- dragons-api/src/pages/loginPage/index.jsx
+++ dragons-api/src/pages/loginPage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import home from './dragon.home.png';
 import '../theme.css';
 import './login.css';
@@ -7,6 +7,14 @@ import './login.css';
 
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (evt) => {
+        evt.preventDefault();
+
+        console.log('submit', {email, password});
+    };
     return (
         <div id="login" className="App-background flex w100">
             <div className="block w50 jContentCenter">
@@ -14,15 +22,29 @@ const LoginPage = () => {
                 <p>choose your dragons!</p>
             </div>
             <div id="form" className="block w50 jContentCenter">
-                <form action="" className="loginForm w100">
+                <form action="" className="loginForm w100" onSubmit={handleLogin}>
                     <div id="userLogin" className="field">
                         <label htmlFor="userName"></label>
-                        <input type="email" className="userName" name="userName" placeholder="email" />
+                        <input
+                            type="email"
+                            id="userName"
+                            name="userName"
+                            placeholder="email"
+                            value={email}
+                            onChange={(evt) => setEmail(evt.target.value)}
+                        />
                     </div>
 
                     <div id="userPassword" className="field">
                         <label htmlFor="userPassword"></label>
-                        <input type="password" className="userPassword" name="userPassword" placeholder="password"/>
+                        <input 
+                            type="password"
+                            id="userPassword"
+                            name="userPassword"
+                            placeholder="password"
+                            value={password}
+                            onChange={(evt) => setPassword(evt.target.value)}
+                        />
                     </div>
 
                     <div className="actions">
